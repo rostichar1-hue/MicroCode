b909c1f212333aa5a3c14fcec00edec7bc7460d7
rostichar <rostichar1@gmail.com>
2026-07-22 18:39:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name='mc-lang',
     version='1.0.0',
-    description='The shortest and most secure query language',
+    description='MicroCode — самый короткий язык запросов',
     author='rostichar1-hue',
     url='https://github.com/rostichar1-hue/MicroCode',
     packages=find_packages(),
-    install_requires=[
-        'tabulate',
-    ],
-    entry_points={
-        'console_scripts': [
-            'micro=microcode.cli:main',
-        ],
-    },
+    install_requires=['tabulate'],
+    entry_points={'console_scripts': ['micro=microcode.cli:main']},
     python_requires='>=3.6',
 )
 EOF
